2f75e7b713ce3dd288ae326687c0b04a76122bdd
kouki <kouworld0123@gmail.com>
2020-04-11 07:26:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# NETWORK-MONITORING
+
+家のネットワークを監視したかった，
+寸断起こりすぎて禿げてるので，自分がどれくらい禿げてるのか確認するためのリポジトリ
+
+ICMPの使い方の参考
+https://mattn.kaoriya.net/software/lang/go/20180315230112.htm
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"golang.org/x/net/ipv4"
+
+	"golang.org/x/net/icmp"
+)
+
+func main() {
+	fmt.Println("Hello, world!")
+	var sleep time.Duration
+	var timeout time.Duration
+
+	flag.DurationVar(&sleep, "s", 1*time.Millisecond, "sleep")
+	flag.DurationVar(&timeout, "t", 100*time.Millisecond, "timeout")
+	flag.Parse()
+
+	proto := "ip4"
+	host := "kmdkuk.com"
+
+	ip, err := net.ResolveIPAddr(proto, host)
+	if err != nil {
+		log.Fatalf("ResolveIPAddr: %v", err)
+	}
+
+	c, err := icmp.ListenPacket(proto+":icmp", "0.0.0.0")
+	if err != nil {
+		log.Fatalf("ListenPacket: %v", err)
+	}
+	defer c.Close()
+
+	for i := 1; i <= 1; i++ {
+		wm := icmp.Message{
+			Type: ipv4.ICMPTypeEcho,
+			Code: 0,
+			Body: &icmp.Echo{
+				ID: os.Getpid() & 0xffff, Seq: i,
+				Data: []byte("HELLO-R-U-THERE"),
+			},
+		}
+		wb, err := wm.Marshal(nil)
+		if err != nil {
+			log.Fatalf("Marshal: %v", err)
+		}
+		if _, err := c.WriteTo(wb, &net.IPAddr{IP: ip.IP}); err != nil {
+			log.Fatalf("WriteTo: %v", err)
+		}
+
+		c.SetReadDeadline(time.Now().Add(timeout))
+		rb := make([]byte, 1500)
+		n, _, err := c.ReadFrom(rb)
+		if err != nil {
+			fmt.Print("U")
+		} else {
+			rm, err := icmp.ParseMessage(ipv4.ICMPTypeEcho.Protocol(), rb[:n])
+			if err == nil && rm.Type == ipv4.ICMPTypeEchoReply {
+				fmt.Print("!")
+			} else {
+				fmt.Print("U")
+			}
+		}
+		if i%70 == 0 {
+			fmt.Println()
+		}
+		time.Sleep(sleep)
+	}
+}
