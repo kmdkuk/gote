cd2ae9210e8b78faa137a227eff569cc1bb5832e
kouki <kouworld0123@gmail.com>
2020-05-19 09:41:49 +0900

    :+1: httpリクエストに対応 (#6)

--- network/http.go
+++ network/http.go
@@ -1,1 +1,18 @@
 package network
+
+import (
+	"log"
+	"net/http"
+)
+
+func sendHttp(url string) bool {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println("[Err]http.Get", err)
+		return false
+	}
+	if resp.StatusCode == http.StatusOK {
+		return true
+	}
+	return false
+}
